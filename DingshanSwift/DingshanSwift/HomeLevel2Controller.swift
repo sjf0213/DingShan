5dd9e36218158044a943e9e956003d32b81c2865
sjf0213 <sjf0213@163.com>
2015-07-24 19:01:07 +0800

    use son data

--- DingshanSwift/DingshanSwift/HomeLevel2Controller.swift
+++ DingshanSwift/DingshanSwift/HomeLevel2Controller.swift
@@ -22,7 +22,7 @@ class HomeLevel2Controller:UIViewController,UITableViewDelegate
         didSet {
             oldValue?.cancel()
             
-            self.title = self.request?.description
+//            self.title = self.request?.description
 //            self.refreshControl?.endRefreshing()
             self.headers.removeAll()
             self.body = nil
@@ -33,7 +33,7 @@ class HomeLevel2Controller:UIViewController,UITableViewDelegate
     {
         super.loadView()
         self.view.backgroundColor = UIColor.lightGrayColor()
-        
+//        self.title = "首页二级"
         self.tableSource = ArrayDataSource(withcellIdentifier: HomeCellIdentifier, configureCellBlock:{(cell, data) in
             let itemCell:HomeLevel2Cell? = cell as? HomeLevel2Cell
             let itemDic:NSDictionary? = data as? NSDictionary
@@ -44,6 +44,7 @@ class HomeLevel2Controller:UIViewController,UITableViewDelegate
         mainTable.backgroundColor = UIColor.yellowColor().colorWithAlphaComponent(0.2)
         mainTable.delegate = self
         mainTable.dataSource = self.tableSource
+        mainTable.rowHeight = 90.0
         mainTable.registerClass(HomeLevel2Cell.classForCoder(), forCellReuseIdentifier: HomeCellIdentifier)
         self.view.addSubview(mainTable)
     }
@@ -56,38 +57,40 @@ class HomeLevel2Controller:UIViewController,UITableViewDelegate
     
     func startRequest()
     {
-
-//        var urlStr:String = "http://v3.kuaigame.cn/app/getcategoryarticle?uid=220151&clientid=21&did=0BBE169C-1528-403E-A418-860FED9AE816&aid=IS4qqOHFBCOKL4jLgGaIWaEGxyI%3D&appver=3.2.1&e=1437633823&iosver=8.4&key=zXxf%2BJwlgISl8rOXfFqsXxuw39s%3D&device=iPhone5%2C2&categoryid=3&pindex=0&psize=20&json=1"
 //        var urlStr:String = "http://v3.kuaigame.com/app/getcategoryarticle?uid=220154&device=iPhone5%2C2&pindex=0&psize=20&appver=3.2.1&key=cNCS0ipHRcFXsuW%2FTyO%2FN%2BmoHsk%3D&did=CD1FBB97-426F-4A83-90AB-A897D580BED2&e=1437637766&categoryid=3&clientid=21&aid=W%2Fsuzn3p2Tb3fQRp1ZaRxZlueKo%3D&iosver=8.4";
         var parameter = ["categoryid" : "3",
                             "pindex" : "0",
-                            "psize" : "20"]
+                            "psize" : "20",
+                            "json" : "1"]
+        var useJson = true
         var url = ApiBuilder.article_get_list(parameter)
         print("url = \(url)")
         self.request = Alamofire.request(.GET, url)
         
-        // JSON
-        /*self.request?.responseJSON(options: .AllowFragments, completionHandler: { (requst1:NSURLRequest, response1:NSHTTPURLResponse?, data:AnyObject?,err: NSError?) -> Void in
+        if useJson == true {
+            // JSON
+            self.request?.responseJSON(options: .AllowFragments, completionHandler: { (requst1:NSURLRequest, response1:NSHTTPURLResponse?, data:AnyObject?,err: NSError?) -> Void in
 
-            print("\n responseJSON- - - - -data = \(data)")
-            print("\n responseJSON- - - - -err = \(err)")
-            if data is NSDictionary
-            {
-                print("\n responseJSON- - - - -data is NSDictionary")
-                self.processRequestResult(data as! NSDictionary)
-            }
-        })*/
-        
-        // Message Pack
-        self.request?.response({ (requst1:NSURLRequest, response1:NSHTTPURLResponse?, responseObject:AnyObject?, err:NSError?) -> Void in
-            if responseObject is NSData{
-                var result: AnyObject? = responseObject?.messagePackParse()
-                print("\n result- - -\(result)")
-                if result is NSDictionary{
-                    self.processRequestResult(result as! NSDictionary)
+                print("\n responseJSON- - - - -data = \(data)")
+                print("\n responseJSON- - - - -err = \(err)")
+                if data is NSDictionary
+                {
+                    print("\n responseJSON- - - - -data is NSDictionary")
+                    self.processRequestResult(data as! NSDictionary)
                 }
-            }
-        })
+            })
+        }else{
+            // Message Pack
+            self.request?.response({ (requst1:NSURLRequest, response1:NSHTTPURLResponse?, responseObject:AnyObject?, err:NSError?) -> Void in
+                if responseObject is NSData{
+                    var result: AnyObject? = responseObject?.messagePackParse()
+                    print("\n result- - -\(result)")
+                    if result is NSDictionary{
+                        self.processRequestResult(result as! NSDictionary)
+                    }
+                }
+            })
+        }
     }
     
     func processRequestResult(result:NSDictionary)
