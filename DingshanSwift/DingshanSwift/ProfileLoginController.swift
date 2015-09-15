557b2920410dfb2ca6ecb9444f947b209344d36d
sjf0213 <sjf0213@163.com>
2015-09-15 18:24:40 +0800

    登录页

---
+++ DingshanSwift/DingshanSwift/ProfileLoginController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileLoginController.swift
+//  DingshanSwift
+//
+//  Created by song jufeng on 15/9/15.
+//  Copyright (c) 2015年 song jufeng. All rights reserved.
+//
+
+import Foundation
+class ProfileLoginController: DSViewController {
+    override func loadView()
+    {
+        super.loadView()
+        self.view.backgroundColor = UIColor.whiteColor()
+    }
+}
--- DingshanSwift/DingshanSwift/ProfileViewController.swift
+++ DingshanSwift/DingshanSwift/ProfileViewController.swift
@@ -10,22 +10,28 @@ import UIKit
 
 class ProfileViewController:DSViewController
 {
+    var mainTable = UITableView()
+    var infoView = UIView()
     override func loadView()
     {
         super.loadView()
         self.view.backgroundColor = UIColor.whiteColor()
         
-        var btn1 = UIButton(frame: CGRect(x:100,y:100,width:100,height:100));
-        btn1.backgroundColor = UIColor.lightGrayColor()
-        btn1.setTitle("go detail", forState: UIControlState.Normal)
-        btn1.addTarget(self, action:Selector("onTapBtn:"), forControlEvents: UIControlEvents.TouchUpInside)
-        self.view.addSubview(btn1)
+        mainTable.frame = self.view.bounds
+        mainTable.contentInset = UIEdgeInsets(top: HomeAd_H, left: 0, bottom: 0, right: 0)
+        self.view.addSubview(self.mainTable)
+        
+        infoView.frame = CGRect(x: 0, y: 0-self.mainTable.contentInset.top, width: self.mainTable.bounds.width, height: self.mainTable.contentInset.top)
+        self.mainTable.addSubview(infoView)
+        
+        var loginBtn = UIButton(frame: CGRect(x:(self.view.bounds.size.width - 100)*0.5, y:100, width:100, height:50));
+        loginBtn.backgroundColor = UIColor.lightGrayColor()
+        loginBtn.setTitle("login", forState: UIControlState.Normal)
+        loginBtn.addTarget(self, action:Selector("onTapLogin:"), forControlEvents: UIControlEvents.TouchUpInside)
+        infoView.addSubview(loginBtn)
     }
     
-    func onTapBtn(sender:UIButton) {
-        print(sender)
-        var detail = DetailController()
-        self.navigationController?.pushViewController(detail, animated: true)
-        
+    func onTapLogin(sender:UIButton) {
+        print("onTapLogin")
     }
 }
