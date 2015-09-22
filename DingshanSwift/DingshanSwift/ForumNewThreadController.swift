fd24371be3fb35e0558193983471ce28a6621eda
sjf0213 <sjf0213@163.com>
2015-09-22 19:25:45 +0800

    新话题

--- DingshanSwift/DingshanSwift/ForumNewThreadController.swift
+++ DingshanSwift/DingshanSwift/ForumNewThreadController.swift
@@ -13,6 +13,7 @@ class ForumNewThreadController : DSViewController{
     
     private var sendBtn = UIButton()
     private var titleTextField = UITextField()
+    private var contentTextView = UITextView()
     var request: Alamofire.Request? {
         didSet {
             oldValue?.cancel()
@@ -25,7 +26,7 @@ class ForumNewThreadController : DSViewController{
         self.topTitle = "新话题"
         
         sendBtn = UIButton(frame: CGRect(x: self.view.bounds.size.width - 44, y: 20, width: 44, height: 44))
-        self.view.bounds.size.width - 2*edge_w
+        sendBtn.backgroundColor = UIColor.blueColor().colorWithAlphaComponent(0.5)
         self.topView.addSubview(sendBtn)
         sendBtn.addTarget(self, action: Selector("onTapSend"), forControlEvents: UIControlEvents.TouchUpInside)
         
@@ -39,7 +40,6 @@ class ForumNewThreadController : DSViewController{
         self.view.addSubview(titleLabel)
         
         titleTextField = UITextField(frame: CGRect(x: edge_w + titleLabel.bounds.width, y: TopBar_H+btnSelectSection.bounds.height, width: self.view.bounds.size.width - 2*edge_w - titleLabel.bounds.width, height: 40))
-//        titleTextField.backgroundColor = UIColor.orangeColor().colorWithAlphaComponent(0.1)
         titleTextField.text = ""
         titleTextField.font = UIFont.systemFontOfSize(15.0)
         self.view.addSubview(titleTextField)
@@ -47,6 +47,13 @@ class ForumNewThreadController : DSViewController{
         let line1 = UIView(frame: CGRect(x: edge_w, y: titleTextField.frame.origin.y + titleTextField.bounds.height, width: self.view.bounds.size.width - 2*edge_w , height: 0.5))
         line1.backgroundColor = UIColor(white: 216/255.0, alpha: 1.0)
         self.view.addSubview(line1)
+        
+        let cy = line1.frame.origin.y + line1.frame.size.height
+        contentTextView = UITextView(frame: CGRect(x: edge_w, y: cy, width: self.view.bounds.size.width - 2*edge_w, height: self.view.bounds.size.height - cy))
+        contentTextView.backgroundColor = UIColor.orangeColor().colorWithAlphaComponent(0.2)
+        contentTextView.text = ""
+        contentTextView.font = UIFont.systemFontOfSize(15.0)
+        self.view.addSubview(contentTextView)
     }
     
     func onTapSend(){
@@ -56,19 +63,21 @@ class ForumNewThreadController : DSViewController{
     func startRequest(){
         let parameter = ["ctype" : "1",
                           "json" : "1"]
-        let postBody = ["topic_title":"sjf_test_topic_title",
-                        "topic_content":"sjf_test_topic_content"]
-        
-        
-        let url = ApiBuilder.forum_create_topic(parameter)
-        
-        do{
-            let theJSONData = try NSJSONSerialization.dataWithJSONObject(postBody, options: NSJSONWritingOptions(rawValue: 0))
-            let theJSONText = NSString(data: theJSONData, encoding: NSASCIIStringEncoding)
-            print("\ntheJSONText = \(theJSONText)")
-            self.request = Alamofire.upload(.POST, url, data: theJSONData)
-        }catch{
-            print(error)
+        let strTitle = titleTextField.text
+        let strContent = contentTextView.text
+        if (strTitle != nil && strContent != nil){
+            let postBody = ["topic_title":strTitle,
+                          "topic_content":strContent]
+            let url = ApiBuilder.forum_create_topic(parameter)
+            
+            do{
+                let theJSONData = try NSJSONSerialization.dataWithJSONObject(postBody, options: NSJSONWritingOptions(rawValue: 0))
+                let theJSONText = NSString(data: theJSONData, encoding: NSASCIIStringEncoding)
+                print("\ntheJSONText = \(theJSONText)")
+                self.request = Alamofire.upload(.POST, url, data: theJSONData)
+            }catch{
+                print(error)
+            }
         }
     }
 }
