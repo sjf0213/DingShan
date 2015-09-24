fa45919b10acccefdbf4f38134694f6612d33c24
sjf0213 <sjf0213@163.com>
2015-09-24 10:05:21 +0800

    user data model

--- DingshanSwift/DingshanSwift/UserInfoData.swift
+++ DingshanSwift/DingshanSwift/UserInfoData.swift
@@ -9,4 +9,16 @@
 import Foundation
 class UserInfoData : NSObject {
     
+    // 数据
+    var uid:Int = 0
+    var aid:String = ""
+    
+    // 可显示资料
+    var userName:String = ""//用户昵称
+    var userHeadUrl:String = ""//用户头像
+    
+    var userGender:String = ""//用户性别
+    var userEmailAddr:String = ""//用户邮箱
+    var userPhoneNum:String = ""//用户手机号
+    
 }
