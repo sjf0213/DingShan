9310a51fb8415a01c6135df1a4de8c70b26a0f68
sjf0213 <sjf0213@163.com>
2015-09-24 15:55:45 +0800

    user info edit

--- DingshanSwift/DingshanSwift/ProfileUserInfoEditController.swift
+++ DingshanSwift/DingshanSwift/ProfileUserInfoEditController.swift
@@ -8,9 +8,21 @@
 
 import Foundation
 class ProfileUserInfoEditController : DSViewController{
+    
+    private var userHeadView = UIButton()
+    
     override func loadView(){
         super.loadView()
-        self.view.backgroundColor = UIColor(red: 0.1, green: 0.2, blue: 0.1, alpha: 1.0)
+        self.view.backgroundColor = UIColor(red: 0.9, green: 0.8, blue: 0.9, alpha: 1.0)
+        
+        userHeadView.frame = CGRect(x: (self.view.bounds.size.width - 73)*0.5, y: 50, width: 73, height: 73)
+        userHeadView.backgroundColor = UIColor.redColor().colorWithAlphaComponent(0.2)
+        userHeadView.layer.cornerRadius = userHeadView.bounds.width * 0.5
+        userHeadView.addTarget(self, action: Selector("onTapUploadHead:"), forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(userHeadView)
+    }
+    
+    func onTapUploadHead(sender:UIButton) {
         
     }
 }
