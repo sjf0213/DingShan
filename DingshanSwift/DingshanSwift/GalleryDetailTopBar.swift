d7df69e57d9eeb8894a692ff4b012460700e724c
sjf0213 <sjf0213@163.com>
2015-11-23 17:44:21 +0800

    切图

--- DingshanSwift/DingshanSwift/GalleryDetailTopBar.swift
+++ DingshanSwift/DingshanSwift/GalleryDetailTopBar.swift
@@ -9,13 +9,26 @@
 import Foundation
 
 class GalleryDetailTopBar : UIView{
-    
+    var backBlock: (() -> Void)? // 最简单的闭包，用来navi返回按钮
     // MARK: init
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     override init(frame aRect: CGRect) {
         super.init(frame: aRect);
-        self.backgroundColor = UIColor.greenColor().colorWithAlphaComponent(0.3);
+        self.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.5);
+        
+        let backBtn = UIButton(frame: CGRect(x: 0, y: 20, width: 44, height: 44))
+        backBtn.backgroundColor = UIColor.clearColor()
+        backBtn.setTitle(String.fontAwesomeIconStringForIconIdentifier("fa-angle-left"), forState: UIControlState.Normal)
+        backBtn.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        backBtn.titleLabel?.font = UIFont(name: "FontAwesome", size: 28)
+        self.addSubview(backBtn)
+        backBtn.addTarget(self, action: Selector("onTapBack"), forControlEvents: UIControlEvents.TouchUpInside)
+    
+    }
+    
+    func onTapBack() {
+        self.backBlock?()
     }
 }
